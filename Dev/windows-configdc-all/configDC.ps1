9495f0fdcbfc4a398fcc6e3cb78ae9e4c9edaabc
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-04 15:13:30 -0500

    added config dc-all

--- Dev/windows-configdc-all/configDC.ps1
+++ Dev/windows-configdc-all/configDC.ps1
@@ -1,13 +1,75 @@
-﻿# Turn off IE Enhanced Security Configuration
+﻿param(
+      [Parameter(Mandatory = $true,valueFromPipeline=$true)]
+	  [String] $OUName,
+      [Parameter(valueFromPipeline=$true)]
+	  [String] $OUDescription
+)
+
+###################################################################################################
+
+#
+# PowerShell configurations
+#
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+# Ensure we set the working directory to that of the script.
+pushd $PSScriptRoot
+
+###################################################################################################
+
+#
+# Functions used in this script.
+#
+
+function Handle-LastError
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
+
+###################################################################################################
+
+#
+# Handle all errors in this script.
+#
+
+trap
+{
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    #       script, unless you want to ignore a specific error.
+    Handle-LastError
+}
+
+###################################################################################################
+
+#
+# Main execution block.
+#
+
+try
+{
+# Turn off IE Enhanced Security Configuration
 $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
 Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
 Stop-Process -Name Explorer
-
-# Install Active Directory Users and Computers
-Import-Module ServerManager
-Add-WindowsFeature RSAT-ADDS-Tools
 
 #Copy Bginfo config file to sysinternals directory & place shortcut in StartUp folder
 Copy-Item .\Bginfo.bgi "C:\ProgramData\chocolatey\lib\sysinternals\tools\Bginfo.bgi"
@@ -141,20 +203,27 @@ foreach ($User in $Users)
 $users = import-csv regional.csv -Delimiter "," -Header Name,FirstName,Password
 foreach ($User in $Users)
 {  
-    New-ADOrganizationalUnit -Name Regional -Description "Regional Users" -PassThru  
-    $OU = "OU = Regional," + $LDAPPath   
-    $Password = $User.password 
-    $Detailedname = $User.firstname + " " + $User.name 
-    $UserFirstname = $User.Firstname 
-    $FirstLetterFirstname = $UserFirstname.substring(0,1) 
-    $SAM =  $FirstLetterFirstname + $User.name 
-    $Description = "Regional user"
-    $Department = "Regional"
-    $Mobile = "123-456-7890"
-    $Telephone = "123-456-7890"
-    $Office = "Regional"
-    New-ADUser -Name $Detailedname -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $Detailedname -GivenName $user.firstname -Surname $user.name -Department $Department -Description $Description -Office $Office -mobile $Mobile -OfficePhone $Telephone -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path $OU  
+   New-ADOrganizationalUnit -Name Regional -Description "Regional Users" -PassThru  
+   $OU = "OU = Regional," + $LDAPPath   
+   Password = $User.password 
+   $Detailedname = $User.firstname + " " + $User.name 
+   $UserFirstname = $User.Firstname 
+   $FirstLetterFirstname = $UserFirstname.substring(0,1) 
+   $SAM =  $FirstLetterFirstname + $User.name 
+   $Description = "Regional user"
+   $Department = "Regional"
+   $Mobile = "123-456-7890"
+   $Telephone = "123-456-7890"
+   $Office = "Regional"
+   New-ADUser -Name $Detailedname -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $Detailedname -GivenName $user.firstname -Surname $user.name -Department $Department -Description $Description -Office $Office -mobile $Mobile -OfficePhone $Telephone -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path $OU  
 } 
 
-
-Exit
+# Install Active Directory Users and Computers
+Import-Module ServerManager
+Add-WindowsFeature RSAT-ADDS-Tools
+	
+}
+finally
+{
+    popd
+}
