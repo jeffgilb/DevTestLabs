4ed80a6af17efed346e26748f19c46322afab19f
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-05 16:20:50 -0500

    updated scripts

--- Dev/windows-populateadds/populateadds.ps1
+++ Dev/windows-populateadds/populateadds.ps1
@@ -6,7 +6,7 @@
 
 # NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
 #       This is necessary to ensure we capture errors inside the try-catch-finally block.
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "SilentlyContinue"
 
 # Ensure we set the working directory to that of the script.
 pushd $PSScriptRoot
