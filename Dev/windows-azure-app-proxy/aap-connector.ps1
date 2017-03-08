08504dd4db1e2ad06c8e8f5b63d9b3d43aee5ef3
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-07 19:57:36 -0500

    added Azure app proxy

--- Dev/windows-azure-app-proxy/aap-connector.ps1
+++ Dev/windows-azure-app-proxy/aap-connector.ps1
@@ -59,13 +59,16 @@ trap
 try
 {
     # Install Azure AD Application Proxy Connector
-	.\AADApplicationProxyConnectorInstaller.exe REGISTERCONNECTOR="false" /q
+	#.\AADApplicationProxyConnectorInstaller.exe REGISTERCONNECTOR="false" /q
 	write-host "Installed Azure application proxy connector."
 
     # Get Azure AD tenant global admin credentials
-	#$User = "jeff@jeffgilb.com"
-	#$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
-	$cred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $User, $Password
+	#$password=$pass|ConvertTo-SecureString -AsPlainText -Force
+	#$Cred = New-Object System.Management.Automation.PsCredential("$uname@domain.tld",$password)
+	
+	$SecurePassword = $Password|ConvertTo-SecureString -AsPlainText -Force
+	#original: $cred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $User, $Password
+	$cred = New-Object System.Management.Automation.PSCredential ($user,$password)
 	write-host "Trying $user to connect to Azure AD."
     
     # Give the install a pause to complete
