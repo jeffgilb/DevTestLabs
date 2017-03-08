f487dde07c3b3cf7cf3acc37eaef6786757b5df3
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-07 21:23:32 -0500

    Moved artifacts from dev to prod

--- Artifacts/windows-autostartbginfo/bginfo.ps1
+++ Artifacts/windows-autostartbginfo/bginfo.ps1
@@ -1,4 +1,57 @@
+###################################################################################################
+# PowerShell configurations
+#
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+# Ensure we set the working directory to that of the script.
+pushd $PSScriptRoot
+
+###################################################################################################
+
+#
+# Functions used in this script.
+#
+
+function Handle-LastError
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
+###################################################################################################
+# Handle all errors in this script.
+#
+trap
+{
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    #       script, unless you want to ignore a specific error.
+    Handle-LastError
+}
+###################################################################################################
+# Main execution block.
+try
+{
 #Copy Bginfo config file to sysinternals directory & place shortcut in StartUp folder
 Copy-Item .\Bginfo.bgi "C:\ProgramData\chocolatey\lib\sysinternals\tools\Bginfo.bgi"
 Copy-Item .\Bginfo.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-Exit
+}
+
+finally
+{
+    popd
+}
