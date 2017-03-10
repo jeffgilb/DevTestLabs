399b355af4042983bc4055042f99384616304e9e
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-10 18:41:48 -0500

    final push for dev10

---
+++ Scripts/enableDirectoryIntegration.ps1
@@ -0,0 +1,11 @@
+﻿	
+    $User = "name"
+    $Password = "password"
+    
+    $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+	$msolcred = New-Object –TypeName System.Management.Automation.PSCredential ($User,$SecurePassword)
+	connect-msolservice -credential $msolcred
+	write-host "Trying $User to connect to Azure AD."
+	connect-msolservice -credential $msolcred
+    
+    Set-MsolDirSyncEnabled –EnableDirSync $true -Force
