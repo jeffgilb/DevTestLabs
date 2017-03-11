334388b96e04722e959620833a48175f89a70e41
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-11 00:32:39 -0500

    install aad ps1

--- Dev/azure-install-cmdlets/install-aadcmdlets.ps1
+++ Dev/azure-install-cmdlets/install-aadcmdlets.ps1
@@ -57,43 +57,11 @@ trap
 
 try
 {
-    # Install Microsoft Online Services Sign-In Assistant for IT Professionals RTW from http://go.microsoft.com/fwlink/?LinkID=286152
-    $Uri="https://www.microsoft.com/en-us/download/confirmation.aspx?id=41950"
-    $Path="C:\Packages\msoidcli_64.msi"
-    $TimeoutSec=30
-    # Ensure the path is available.
-    if (-not [System.IO.Path]::IsPathRooted($Path))
-    {
-        $Path = Join-Path $Env:Temp $Path
-    }
-    Write-Host "Ensuring local path $Path"
-    New-Item -ItemType Directory -Force -Path (Split-Path -parent $Path) | Out-Null
-
-    # Download requested file.
-    Write-Host "Downloading Microsoft Online Services Sign-In Assistant for IT Professionals from $Uri to C:\Packages\."
-    Invoke-WebRequest -Uri $Uri -OutFile $Path -TimeoutSec $TimeoutSec
-
-   # Install Azure AD PowerShell cmdlets http://go.microsoft.com/fwlink/p/?linkid=236297
-    $Uri="http://go.microsoft.com/fwlink/p/?linkid=236297"
-    $Path="C:\Packages\AdministrationConfig-en.msi"
-    $TimeoutSec=30
-    # Ensure the path is available.
-    if (-not [System.IO.Path]::IsPathRooted($Path))
-    {
-        $Path = Join-Path $Env:Temp $Path
-    }
-    Write-Host "Ensuring local path $Path"
-    New-Item -ItemType Directory -Force -Path (Split-Path -parent $Path) | Out-Null
-
-    # Download requested file.
-    Write-Host "Downloading Azure AD PowerShell cmdlets from $Uri to C:\Packages\."
-    Invoke-WebRequest -Uri $Uri -OutFile $Path -TimeoutSec $TimeoutSec
-
-   # Kick off the installs for all users.
-    C:\Packages\AdministrationConfig-en.msi /qb
+   # Install Microsoft Online Services Sign-In Assistant for IT Professionals RTW from http://go.microsoft.com/fwlink/?LinkID=286152
+    .\msoidcli_64.msi /passive ALLUSERS=1
     Write-Host "Successfully installed Microsoft Online Services Sign-In Assistant."
-    
-    C:\Packages\AdministrationConfig-en.msi /qb
+   # Install Azure AD PowerShell cmdlets http://go.microsoft.com/fwlink/p/?linkid=236297
+    .\AdministrationConfig-en.msi /passive ALLUSERS=1
     Write-Host "Successfully installed Azure AD PowerShell cmdlets."
 }
 finally
