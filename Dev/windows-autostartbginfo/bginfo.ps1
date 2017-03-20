cffa762c3594501389d3f9121b18bb865c4131c2
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-20 16:48:17 -0400

    cleaned up repo

--- Dev/windows-autostartbginfo/bginfo.ps1
+++
@@ -1,4 +0,0 @@
-#Copy Bginfo config file to sysinternals directory & place shortcut in StartUp folder
-Copy-Item .\Bginfo.bgi "C:\ProgramData\chocolatey\lib\sysinternals\tools\Bginfo.bgi"
-Copy-Item .\Bginfo.lnk "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
-Exit
