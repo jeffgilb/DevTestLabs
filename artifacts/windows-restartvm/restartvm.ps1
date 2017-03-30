ef8cee69d39f46a779d1412e0d06b503503a3d1b
Jeff Gilbert <jeffgilb@microsoft.com>
2017-03-30 19:58:39 -0400

    Updated some scripts
    sdaf

--- artifacts/windows-restartvm/restartvm.ps1
+++ artifacts/windows-restartvm/restartvm.ps1
@@ -46,9 +46,11 @@ trap
 # Main execution block.
 try
 {
-# Restarts VM
-# This will show up in the System event log as Event ID 1074
-shutdown /r /t 0 /c "Restart required and initiated by Azure DevTestLabs artifact." /d p:4:2
+    # Restarts VM
+	restart-computer
+
+    # Or you can do it this way and it will show up in the System event log as Event ID 1074
+    #	shutdown /r /t 0 /c "Restart required and initiated by Azure DevTestLabs artifact." /d p:4:2
 }
 
 finally
